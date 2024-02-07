e02e5201da383db318a81d18b224cda154c7cf55
lokloter <lokloter@gmail.com>
2024-02-07 11:50:18 +0200

    додав фон та зображення

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
-    type: Phaser.AUTO, //встанвлює контекст візуалізації, який ми хочемо використовувати для своєї гри
-    width: 800,  //встановлює розмір
-    height: 600,
+  type: Phaser.AUTO, //встанвлює контекст візуалізації, який ми хочемо використовувати для своєї гри
+  width: 800, //встановлює розмір
+  height: 600,
   scene: {
     preload: preload,
     create: create,
@@ -11,8 +11,28 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  //завантажує ресурси для гри
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
 
-function create() {}
+function create() {
+  this.add.image(400, 300, "sky"); //відображає ресурси гри в саму гру
+
+  платформи = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+}
 
 function update() {}
