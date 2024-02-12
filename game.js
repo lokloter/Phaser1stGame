6cffb66a2b8dbd45223ff45a3831a50ceccffdb4
lokloter <lokloter@gmail.com>
2024-02-12 09:21:17 +0200

    bombs

--- game.js
+++ game.js
@@ -88,9 +88,57 @@ function create() {
   function collectStar(player, star) {
     star.disableBody(true, true);
   }
+  var score = 0;
+  var scoreText;
+  scoreText = this.add.text(16, 16, "score: 0", {
+    fontSize: "32px",
+    fill: "#000",
+  });
+  function collectStar(player, star) {
+    star.disableBody(true, true);
 
+    score += 10;
+    scoreText.setText("Score: " + score);
+  }
   S;
   cursors = this.input.keyboard.createCursorKeys();
+  bombs = this.physics.add.group();
+
+  this.physics.add.collider(bombs, platforms);
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+  function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play("turn");
+
+    gameOver = true;
+  }
+  function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText("Score: " + score);
+
+    if (stars.countActive(true) === 0) {
+      stars.children.iterate(function (child) {
+        child.enableBody(true, child.x, 0, true, true);
+      });
+
+      var x =
+        player.x < 400
+          ? Phaser.Math.Between(400, 800)
+          : Phaser.Math.Between(0, 400);
+
+      var bomb = bombs.create(x, 16, "bomb");
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
+  }
 }
 
 function update() {
